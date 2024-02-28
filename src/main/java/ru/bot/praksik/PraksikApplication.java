ca904f15c3bbd9d3114ede7a1cbe663f8a3ff862
Vadsxd <vadim.sannikov.2018@mail.ru>
2024-02-28 12:26:24 +0300

    init

---
+++ src/main/java/ru/bot/praksik/PraksikApplication.java
@@ -0,0 +1,13 @@
+package ru.bot.praksik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraksikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraksikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bot/praksik/PraksikApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bot.praksik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraksikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
